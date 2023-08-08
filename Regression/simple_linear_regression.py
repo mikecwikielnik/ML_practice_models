a41c0c96b373914bbb7910bded032bf6d6369eaa
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-08-07 23:06:40 -0400

    Add files via upload

--- Regression/simple_linear_regression.py
+++ Regression/simple_linear_regression.py
@@ -66,3 +66,50 @@ forest.score(X_train, forest_y_predictions)
 print("MSE (loss) for random forest:\n", mean_squared_error(y_train, forest_y_predictions))
 print("MAE (accuracy) for random forest:\n", mean_absolute_error(y_train, forest_y_predictions))
       
+################################################
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(128, input_dim = 1, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
+model.summary()
+
+history = model.fit(X_train, y_train, validation_split=0.2, epochs=100)
+
+import matplotlib.pyplot as plt
+
+loss = history.history['loss'] # training
+val_loss = history.history['val_loss']  # validation
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training Loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+# plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+
+acc = history.history['mae']    # training
+val_acc = history.history['val_mae']    # validation
+plt.plot(epochs, acc, 'y', label='Training MAE (accuracy)')
+plt.plot(epochs, val_acc, 'r', label = 'Validation MAE (accuracy)')
+plt.title = ('Training and Validation MAE (accuracy)')
+plt.xlabel('Epochs')
+plt.ylabel('Mean Squared Error (accuracy)')
+plt.legend()
+plt.show()
+
+# ------- predict on test data -----------
+
+predictions = model.predict(X_test)
+print("Predicted values are:\n", predictions)
+print("Real values are:\n", y_test)
