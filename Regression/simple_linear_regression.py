bd7f30847f120ef2adcce19b0a584af06937d4d4
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-08-07 22:10:41 -0400

    Add files via upload

---
+++ Regression/simple_linear_regression.py
@@ -0,0 +1,32 @@
+"""
+a simple linear regression via a toy nhl dataset
+"""
+
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("data/nhl_goals.csv")
+print(df)
+
+X = df.drop('Goals', axis='columns')
+y = df.Goals # y is an int 64, not an object like X 
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)     # training the model (fitting a line)
+
+y_predictions = model.predict(X_test)
+
+# y_test = y_test.drop('Goal predict', axis='columns')
+
+print(y_test, y_predictions)
+
+model.score(X_train, y_predictions)
+
+print("the mean square error is:\n", mean_squared_error(y_test, y_predictions))
+
+print("the mean absolute error is:\n", mean_absolute_error(y_test, y_predictions))
